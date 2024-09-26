2b3107797230160fb163914b55e39a5124b59bb9
facufr12 <facufr2@hotmail.com>
2024-09-26 11:13:29 -0300

    prd

--- script.js
+++ script.js
@@ -6,10 +6,6 @@ function calculateResult() {
 
     let result = "";
 
-<<<<<<< HEAD
-    // Patologías con GM
-    if (["diabetes", "oncológicas", "trasplantados", "artritis", "hiv"].includes(pathology)) {
-=======
     // Condiciones para epilepsia
     if (pathology === "epilepsia") {
         if (consumption === "yes" && capitas >= 2 && ageRange === "under65") {
@@ -54,7 +50,6 @@ function calculateResult() {
 
     // Otras patologías
     else if (["diabetes", "oncológicas", "trasplantados", "artritis", "hiv"].includes(pathology)) {
->>>>>>> ece0796 (cambiosgiuliana)
         result = "GM";
     } else if (pathology === "obesidad" && consumption === "yes") {
         result = "GM";
@@ -63,58 +58,12 @@ function calculateResult() {
             result = "GC";
         } else {
             result = "CHEQUEAR CON EQUIPO DE FIDELIZACION";
-<<<<<<< HEAD
-        }
-    } else if (pathology === "cud") {
-        result = "GM"; // CUD tiene que ser siempre GM
-    } else if (["celiaquia", "psiquiátricos", "judicializados", "neurológicos"].includes(pathology)) {
-        if (pathology === "celiaquia") {
-            if (consumption === "no" && capitas > 2 && ageRange === "under65") {
-                result = "GC";
-            } else if (consumption === "no" && capitas >= 1 && ageRange === "over65") { 
-                result = "GC";
-            } else if (consumption === "yes" && capitas >= 1) {
-                result = "CHEQUEAR CON FIDELIZACION"; 
-            }
-        } else if (pathology === "epilepsia") {
-            if (consumption === "no" && capitas >= 1) {
-                result = "GC"; 
-            } else if (consumption === "yes") {
-                result = "GM";
-            }
-        } else if (pathology === "neurológicos") {
-            if (consumption === "no" && capitas === 1 && ageRange === "under65") {
-                result = "CHEQUEAR CON FIDELIZACION";
-            } else if (consumption === "yes" && capitas >= 1) {
-                result = "GM";
-            }
-        } else if (pathology === "psiquiátricos" && consumption === "no" && capitas === 1 && ageRange === "under65") {
-            result = "GC"; 
-        } else if (pathology === "judicializados") {
-            if (consumption === "no" && capitas >= 1) {
-                result = "GM"; 
-            } else {
-                result = "GM"; 
-            }
-        }
-    } else if (pathology === "cardiacas") {
-        if (consumption === "no" && capitas > 2 && ageRange === "under65") {
-            result = "GC"; // Nueva condición para enfermedad cardiaca
-        } else {
-            result = "GM"; // En otras condiciones, enfermedad cardiaca es GM
-        }
-=======
         } 
     } else if (pathology === "cud" && consumption === "no") {
         result = "CHEQUEAR CON FIDELIZACION";
->>>>>>> ece0796 (cambiosgiuliana)
     } else {
         result = "GC";
     }
 
     document.getElementById("result").innerText = result;
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> ece0796 (cambiosgiuliana)
