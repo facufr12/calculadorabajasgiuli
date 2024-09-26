390765bb00bd86b88165d23f0bbd0484d4b006e8
facufr12 <facufr2@hotmail.com>
2024-09-26 11:06:43 -0300

    s

--- script.js
+++ script.js
@@ -6,8 +6,55 @@ function calculateResult() {
 
     let result = "";
 
+<<<<<<< HEAD
     // Patologías con GM
     if (["diabetes", "oncológicas", "trasplantados", "artritis", "hiv"].includes(pathology)) {
+=======
+    // Condiciones para epilepsia
+    if (pathology === "epilepsia") {
+        if (consumption === "yes" && capitas >= 2 && ageRange === "under65") {
+            result = "CHEQUEAR CON FIDELIZACION";
+        } else if (consumption === "yes" && capitas >= 1 && ageRange === "under65") {
+            result = "GM";
+        } else if (consumption === "yes" && capitas >= 2) {
+            result = "CHEQUEAR CON FIDELIZACION";
+        }
+    }
+
+    // Condiciones para estudios neurológicos
+    else if (pathology === "neurológicos") {
+        if (consumption === "no" && capitas >= 2) {
+            result = "GC";
+        }
+    }
+
+    // Condiciones para pacientes psiquiátricos
+    else if (pathology === "psiquiátricos") {
+        if (consumption === "no" && capitas >= 2 && ageRange === "over65") {
+            result = "CHEQUEAR CON FIDELIZACION";
+        } else if (consumption === "no" && capitas >= 1) {
+            result = "GC";
+        } else if (consumption === "yes" && capitas >= 2 && ageRange === "under65") {
+            result = "GC";
+        } else if (consumption === "yes" && capitas >= 1 && ageRange === "under65") {
+            result = "CHEQUEAR CON FIDELIZACION";
+        } else if (consumption === "yes" && capitas >= 1 && ageRange === "over65") {
+            result = "GM";
+        }
+    }
+
+    // Condiciones para enfermedades cardíacas
+    else if (pathology === "cardiacas") {
+        if (consumption === "no" && capitas >= 1 && ageRange === "under65") {
+            result = "GC";
+        } else if (consumption === "no" && capitas >= 2 && ageRange === "over65") {
+            result = "GC";
+        }
+    }
+
+    // Otras patologías
+    else if (["diabetes", "oncológicas", "trasplantados", "artritis", "hiv"].includes(pathology)) {
+>>>>>>> ece0796 (cambiosgiuliana)
         result = "GM";
     } else if (pathology === "obesidad" && consumption === "yes") {
         result = "GM";
@@ -16,6 +63,7 @@ function calculateResult() {
             result = "GC";
         } else {
             result = "CHEQUEAR CON EQUIPO DE FIDELIZACION";
+<<<<<<< HEAD
         }
     } else if (pathology === "cud") {
         result = "GM"; // CUD tiene que ser siempre GM
@@ -55,9 +103,18 @@ function calculateResult() {
         } else {
             result = "GM"; // En otras condiciones, enfermedad cardiaca es GM
         }
+=======
+        } 
+    } else if (pathology === "cud" && consumption === "no") {
+        result = "CHEQUEAR CON FIDELIZACION";
+>>>>>>> ece0796 (cambiosgiuliana)
     } else {
         result = "GC";
     }
 
     document.getElementById("result").innerText = result;
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> ece0796 (cambiosgiuliana)
